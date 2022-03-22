40e43344db509702581f067c99af7724529369c0
guswawan <agsn.does6@gmail.com>
2022-03-22 18:12:26 +0700

    add images and write unit tests

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -24,27 +25,49 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollDice()
-
-        // Show toast
-        // val toast = Toast.makeText(this,"Dice Rolled!",Toast.LENGTH_SHORT).show()
         }
+
+        // Do random roll dice when app launch
+        rollDice()
     }
 
     /**
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
-        // Create new Dice 1 and 2 object with 6 sides and 12 sides and roll it
+        // Create new Dice object with 6 sides and roll the dice
         val dice = Dice(6)
-        val dice2 = Dice(12)
         val diceRoll = dice.roll()
-        val diceRoll2 = dice2.roll()
+        val diceRoll2 = dice.roll()
 
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        // Find the ImageView in the layout
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val diceImage2: ImageView = findViewById(R.id.imageView2)
 
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+        // Determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource2 = when (diceRoll2){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        // Update the ImageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
+
+        // Update the content description
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
--- app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
+++ app/src/test/java/com/example/diceroller/ExampleUnitTest.kt
@@ -1,6 +1,7 @@
 package com.example.diceroller
 
 import org.junit.Test
+import org.junit.Assert.assertTrue
 
 import org.junit.Assert.*
 
@@ -10,8 +11,16 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(5, 3 + 2)
+//    }
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generate_number() {
+    val dice = MainActivity.Dice(6)
+    val rollResult = dice.roll()
+        assertTrue("Value rollResult was not between 1 and 6 ",rollResult in 1..6)
     }
+
+
 }
